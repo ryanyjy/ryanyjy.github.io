933462d2ae2287bdabcd503244325cab58af2729
Ryjy <32213416+ryanyjy@users.noreply.github.com>
2023-08-03 17:26:12 +0800

    classNameChange

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 function showContent() {
-  const content = document.getElementById("hidden-content");
+  const content = document.getElementById("tldr-hidden-content");
   content.style.display = content.style.display === "none" ? "" : "none";
 
-  const contentShade = document.getElementById("hidden-content-shade");
-  contentShade.style.display = contentShade.style.display === "none" ? "" : "none";
+  const contentShade = document.getElementById("tldr-hidden-content-shade");
+  contentShade.style.display =
+    contentShade.style.display === "none" ? "" : "none";
 }
